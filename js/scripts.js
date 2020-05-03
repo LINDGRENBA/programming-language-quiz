7556b238b2281343a1acc7a897fc4642e257d193
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-05-02 19:41:19 -0700

    Update js and jQuery with branching and functions. Add details about ruby vs python vs C# in html results div.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,46 @@
 $(document).ready(function() {
   $("#quiz-form").submit(function(event) {
     event.preventDefault();
-
+    // VARIABLES TO HOLD USER INPUT
     let nameInput = $("#name").val();
     let projectTypeInput = $("#project-type").val();
     let salaryInput = $("#salary").val();
     let cityInput = $("input:radio[name=city]:checked").val();
     let employerTypeInput =  $("input:radio[name=employer]:checked").val();
     let spiritAnimalInput = $("#spirit-animal").val();
-    console.log(nameInput);
-    console.log(projectTypeInput);
-    console.log(salaryInput);
-    console.log(cityInput);
-    console.log(employerTypeInput);
-    console.log(spiritAnimalInput);
+
+    // FUNCTIONS TO SHOW RESULTS
+    let showCSharp = function() {
+      $(".c-sharp").show();
+      $(".python").hide();
+      $(".ruby").hide();
+    };
+    let showRuby = function() {
+      $(".ruby").show();
+      $(".python").hide();
+      $(".c-sharp").hide();
+    };
+    let showPython = function() {
+      $(".python").show();
+      $(".ruby").hide();
+      $(".c-sharp").hide();
+    };
+
+    if(projectTypeInput === "windows" && employerTypeInput === "corporate" && spiritAnimalInput === "owl") {
+      showCSharp();
+    } else if(projectTypeInput === "arduino-pi" && employerTypeInput === "startup" && spiritAnimalInput === "unicorn") {
+      showRuby();
+    } else if(projectTypeInput === "machine-ai" && cityInput === "new-york" && salaryInput === "salary-very-important") {
+      showPython();
+    } else if(projectTypeInput === "windows" && cityInput === "seattle" && salaryInput === "salary-not-important") {
+      showCSharp();
+    } else if(salaryInput === "salary-med-important" && cityInput === "new-york" employerTypeInput === "startup") {
+      showRuby();
+    } else {
+      showPython();
+    }
+
+    $(".name-here").text(nameInput);
 
   });
 });
